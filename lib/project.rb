9c87ba9d75f75a872d02a96bc9142c84ae4e25a6
Meg Galvez <meg@oakmaine.com>
2019-05-19 23:12:27 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  
+  attr_accessor :title 
+  attr_reader :backers 
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(backer)
+    @backers << backer 
+    backer.back_project(self) unless backer.backed_projects.include?(backer)
+  end 
+end 
